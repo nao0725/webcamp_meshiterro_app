8a5329e389b6fe38ceeb311747f385216a4a36c1
nao0725 <aqua.725.times@gmail.com>
2021-06-10 13:32:57 +0000

    finish5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+ Refile.secret_key = 'd28a7ca658af795a1bda5ea8e743d89609dc1bc978233d45b345d3b8a0b07867adaed6cee18b169a34eee27219f0f58a837adaae2dfe0ae9b90ef14287c03506'
